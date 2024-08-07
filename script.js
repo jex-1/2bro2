e710f76594e61b54f3fcaf5841c88b617c65c80b
DH6H <73754965+jex-1@users.noreply.github.com>
2024-08-08 02:31:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let header = document.querySelector("header");
+
+window.onscroll = function () {
+  if (this.scrollY >= 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+
+let nav_links = document.getElementById("links");
+
+function Open_clo() {
+  nav_links.classList.toggle("active");
+}
